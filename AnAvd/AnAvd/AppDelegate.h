bed3e63cf3e9fa2de02c90d5d47fc97720e69320
Anurag Sharma <anurag_android@hotmail.com>
2016-06-03 21:41:37 +0530

    P1

---
+++ AnAvd/AnAvd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnAvd
+//
+//  Created by Anurag Sharma on 6/3/16.
+//  Copyright © 2016 Anurag Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
